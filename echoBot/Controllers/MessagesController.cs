537c5f1ec12f07dd217619663e839acdc6ca9e78
fattaneh Ameri Mahabadian <fatameri@hotmail.com>
2018-05-08 15:21:02 -0400

    dialog

--- echoBot/Controllers/MessagesController.cs
+++ echoBot/Controllers/MessagesController.cs
@@ -30,8 +30,33 @@ namespace echoBot
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
+        public class InterviewDialog : IDialog<object>
+        {
+            private int _patience = 0;
 
-        private Activity HandleSystemMessage(Activity message)
+            public async Task StartAsync(IDialogContext context)
+            {
+                _patience = 0;
+              context.Wait(PatientReadAsync);
+            }
+
+            public async Task PatientReadAsync(IDialogContext context, IAwaitable<IActivity> awaitable)
+            {
+                var activity = await awaitable;
+                if (activity.Type == ActivityTypes.Message)
+                {
+                   
+                    context.Wait(PatientReadAsync);
+                }
+                else
+                {
+                    await context.PostAsync("My patience is running out: " + _patience);
+                    context.Wait(PatientReadAsync);
+                }
+            }
+
+        }
+            private Activity HandleSystemMessage(Activity message)
         {
             if (message.Type == ActivityTypes.DeleteUserData)
             {
